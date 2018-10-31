b16d242c3ae60eef5111d7defce170bfbeef8fb9
Cris Kim <isizisiz@gmail.com>
2018-10-30 18:04:16 -0700

    Create class for all entries view controller and connect to story board

---
+++ Activity Tracker/Controller/AllEntriesTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AllEntriesTableViewController.swift
+//  Activity Tracker
+//
+//  Created by CHRISTOPHER KIM on 10/30/18.
+//  Copyright © 2018 Douglas Putnam. All rights reserved.
+//
+import RealmSwift
+import UIKit
+
+class AllEntriesTableViewController: UITableViewController {
+
+
+    
+    
+
+}
