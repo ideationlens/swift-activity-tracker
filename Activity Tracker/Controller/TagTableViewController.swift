82ade8f52474351a381ff6d93df521467af0d0fd
douglas-putnam <douglasputnam@gmail.com>
2018-11-07 08:32:34 -0500

    commit pod files

--- Activity Tracker/Controller/TagTableViewController.swift
+++ Activity Tracker/Controller/TagTableViewController.swift
@@ -48,23 +48,29 @@ class TagTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tags.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = DisclosureCell()
+        cell.title = tags[indexPath.row].name
+        if activity.tags.contains(tags[indexPath.row]) {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
 
         // Configure the cell...
 
         return cell
     }
-    */
+ 
 
     /*
     // Override to support conditional editing of the table view.
