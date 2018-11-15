c28d45cb8964a72329c13ac4b93fa0d31c995c01
douglas-putnam <douglasputnam@gmail.com>
2018-11-14 22:15:54 -0500

    Add ChartCell

---
+++ Activity Tracker/View/ChartCell.swift
@@ -0,0 +1,26 @@
+//
+//  ChartCell.swift
+//  Activity Tracker
+//
+//  Created by Douglas Putnam on 11/14/18.
+//  Copyright © 2018 Douglas Putnam. All rights reserved.
+//
+
+
+import UIKit
+
+class ChartCell: UITableViewCell {
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
